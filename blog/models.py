2e7c998e75681cfd87664cdfceee438e7829a843
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2020-07-30 21:46:14 -0300

    Returning the post title

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    summary = models.CharField(max_length=255)
-    content = models.TextField()
+    summary = RichTextField() # ckeditor Text Field
+    content = RichTextUploadingField() # ckeditor Text Field + upload images
     author = models.ForeignKey(User, on_delete = models.PROTECT) # Protege o user
     created_at = models.DateField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
