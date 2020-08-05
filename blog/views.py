c469543c1f32c9e4314487479e8b603fb621ecb8
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2020-08-05 17:33:46 -0300

    Create index and post views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.all() # taking all posts
+    return render(request, 'index.html', {'posts':posts}) # Retornando o post no template
+
+def post(request, post_id):
+    posts = Post.objects.get(pk=post_id) # getting the specific post
+    return render(request, 'post.html', {'posts':posts})
